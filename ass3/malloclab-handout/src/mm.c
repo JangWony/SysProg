7d78cfe29f74bd4353cf437a2b84befe48ea9298
JangWony <jwon2@snu.ac.kr>
2019-04-22 00:34:16 +0900

    list

--- ass3/malloclab-handout/src/mm.c
+++ ass3/malloclab-handout/src/mm.c
@@ -146,15 +146,15 @@ static void place(void *bp, size_t asize){
 
     if((csize - asize) >= (2*DSIZE)) {
         PUT(HDRP(bp), PACK(asize, 1));
-        PUT(FDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
         bp = NEXT_BLKP(bp);
         PUT(HDRP(bp), PACK(csize-asize,0));
-        PUT(FDRP(bp), PACK(csize-asize,0));
+        PUT(FTRP(bp), PACK(csize-asize,0));
     }
     else{
 
         PUT(HDRP(bp), PACK(csize,1));
-        PUT(FDRP(bp), PACK(csize,1));
+        PUT(FTRP(bp), PACK(csize,1));
     }
 }
 
