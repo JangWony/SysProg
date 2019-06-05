397994fc56f42a1882e3ec17c8f0514f56387f46
JangWony <jwon2@snu.ac.kr>
2019-06-05 21:46:23 +0900

    17

--- ass5/proxylab-handout/proxy.c
+++ ass5/proxylab-handout/proxy.c
@@ -19,11 +19,8 @@ static const char *proxy_connection_key = "Proxy-Connection";
 static const char *host_key = "Host";
 
 void *thread(void *vargp);
-int parse_uri(char *uri, char *hostname, char *query, int *port);
+void parse_uri(char *uri, char *hostname, char *path, int *port);
 void handle(int conn_fd);
-
-ssize_t Rio_readlineb_w(rio_t *rp, void *usrbuf, size_t maxlen);
-void Rio_writeb_w(int fd, void *usrbuf, size_t n);
 
 void build_http_header(char *http_header,char *hostname,char *path,int port,rio_t *client_rio);
 int connect_endServer(char *hostname,int port,char *http_header);
@@ -96,36 +93,36 @@ void *thread(void *vargp) {
     Close(conn_fd);
 }
 
-int parse_uri(char *uri, char *hostname, char *query, int *port){
+void parse_uri(char *uri, char *hostname, char *path, int *port){
 
-    char *hoststart, *hostend, *pathstart;
-    int length;
+    *port = 80;
+    char* pos = strstr(uri,"//");
 
-    if(strncasecmp(uri, "http://", 7)!=0){
-        hostname[0] = '\0';
-        return -1;
-    }
+    pos = pos!=NULL? pos+2:uri;
 
-    hoststart = uri + 7;
-    hostend = strpbrk(hoststart, " :/\rn\n\0");
-    length = hostend - hoststart;
-    strncpy(hostname, hoststart, length);
-    hostname[length] = '\0';
-
-    *port= 80;
-    if(*hostend == ':'){
-        *port = atoi(hostend + 1);
+    char*pos2 = strstr(pos,":");
+    if(pos2!=NULL)
+    {
+        *pos2 = '\0';
+        sscanf(pos,"%s",hostname);
+        sscanf(pos2+1,"%d%s",port,path);
     }
-    
-    pathstart = strchr(hoststart, '/');
-    if(pathstart == NULL){
-        query[0] = '\0';
+    else
+    {
+        pos2 = strstr(pos,"/");
+        if(pos2!=NULL)
+        {
+            *pos2 = '\0';
+            sscanf(pos,"%s",hostname);
+            *pos2 = '/';
+            sscanf(pos2,"%s",path);
+        }
+        else
+        {
+            sscanf(pos,"%s",hostname);
+        }
     }
-    else{
-        pathstart++;
-        strncpy(query, pathstart, MAXLINE);
-    }
-    return 0;
+    return;
 }
 
 void handle(int conn_fd){
@@ -189,22 +186,6 @@ void handle(int conn_fd){
     }
     return;
     
-}
-
-ssize_t Rio_readlineb_w(rio_t *rp, void *usrbuf, size_t maxlen){
-    ssize_t ret;
-    if((ret = rio_readlineb(rp, usrbuf, maxlen)) < 0){
-        printf("Warning: Rio_readlineb error.\n");
-        return 0;
-    }
-    return ret; 
-}
-
-void Rio_writeb_w(int fd, void *usrbuf, size_t n){
-    if(rio_writen(fd, usrbuf, n)!= n){
-        printf("Warning: Rio_writen failed.\n");
-        return;
-    }
 }
 
 void build_http_header(char *http_header,char *hostname,char *path,int port,rio_t *client_rio)
@@ -353,7 +334,7 @@ void cache_LRU(int index){
         writeAfter(i);
     }
     i++;
-    for(i; i<10; i++)    {
+    for(; i<10; i++)    {
         writePre(i);
         if(cache.cacheobjs[i].isEmpty==0 && i!=index){
             cache.cacheobjs[i].LRU--;
