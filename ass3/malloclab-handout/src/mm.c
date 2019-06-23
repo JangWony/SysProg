f5aa911911b84cd9c972499900e8619887118b16
JangWony <jwon2@snu.ac.kr>
2019-06-23 20:32:49 +0900

    d

--- ass3/malloclab-handout/src/mm.c
+++ ass3/malloclab-handout/src/mm.c
@@ -54,16 +54,22 @@
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)- WSIZE)))
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp)- DSIZE)))
 
+#define GET_ALLOC_PREV_BLOCK(bp)   (GET(HDRP(bp)) & 2) 
 
+#define FREE_LIST_ARRAY_SIZE 16
 
 /* Global var */
 char *heap_listp;
+static char **freeListArray = NULL;
 
 /* functions */
 static void *extend_heap(size_t size);
 static void *coalesce(void *bp);
 static void *find_fit(size_t asize);
 static void place(void *bp, size_t asize);
+
+static void addToFreeList(void *bp);
+static void deleteFromFreeList(void *bp);
 
 /* 
  * mm_init - initialize the malloc package.
@@ -72,8 +78,14 @@ int mm_init(void)
 {
 
 
-    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+    if((heap_listp = mem_sbrk(FREE_LIST_ARRAY_SIZE*WSIZE)) == (void *)-1)
         return -1;
+
+    freeListArray = (char **)heap_listp;
+    memset(freeListArray, 0, (FREE_LIST_ARRAY_SIZE*DSIZE));
+
+    heap_listp += (FREE_LIST_ARRAY_SIZE*DSIZE);
+
     PUT(heap_listp, 0);                                     /* Alignment padding */
     PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));            /* Prologue header */
     PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));            /* Prologue footer */
@@ -95,8 +107,8 @@ static void *extend_heap(size_t words){
     if((long) (bp = mem_sbrk(size)) == -1)
         return NULL;
     
-    PUT(HDRP(bp), PACK(size, 0));
-    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(bp), PACK(size, GET_ALLOC_PREV_BLOCK(bp)|0));
+    PUT(FTRP(bp), PACK(size, GET_ALLOC_PREV_BLOCK(bp)|0));
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1));
 
     return coalesce(bp);
@@ -104,27 +116,32 @@ static void *extend_heap(size_t words){
 
 
 static void *coalesce(void *bp){
-    size_t prev_alloc = GET_ALLOC(HDRP(PREV_BLKP(bp)));
+    size_t prev_alloc = GET_ALLOC_PREV_BLOCK(bp));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
 
 
     if (prev_alloc && next_alloc) {             // case 1
+        addToFreeList(bp);
         return bp;
     }
     else if (prev_alloc && !next_alloc) {       // case 2   
+        deleteFromFreeList(NEXT_BLKP(bp));	
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-        PUT(HDRP(bp), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size, 0));
-    } else if (!prev_alloc && next_alloc) {     // case 3            
+        PUT(HDRP(bp), PACK(size, GET_ALLOC_PREV_BLOCK(bp)|0));
+        PUT(FTRP(bp), PACK(size, GET_ALLOC_PREV_BLOCK(bp)|0));
+    } else if (!prev_alloc && next_alloc) {     // case 3  
+        deleteFromFreeList(PREV_BLKP(bp));          
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-        PUT(FTRP(bp), PACK(size, 0));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, GET_ALLOC_PREV_BLOCK(bp)|0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, GET_ALLOC_PREV_BLOCK(bp)|0));
         bp = PREV_BLKP(bp);
     } else {                                    // case 4   
+        deleteFromFreeList(NEXT_BLKP(bp));	
+		deleteFromFreeList(PREV_BLKP(bp));
         size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp)));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, GET_ALLOC_PREV_BLOCK(bp)|0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, GET_ALLOC_PREV_BLOCK(bp)|0));
         bp = PREV_BLKP(bp);
     }
     
@@ -133,29 +150,38 @@ static void *coalesce(void *bp){
 
 static void *find_fit(size_t asize){
     void *bp;
+    int index = indexOfFreeListArray(asize);
 
-    for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0 ; bp = NEXT_BLKP(bp)){
-        if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
-            return bp;
-        }
-    }
-    return NULL;
+    while(index < FREE_LIST_ARRAY_SIZE)
+	{
+		for (bp = freeListArray[index]; bp && GET_SIZE(HDRP(bp)) > 0; 
+				bp = actualAddressFromOffset(GET(NEXT_PTR(bp)))){
+			if (asize <= GET_SIZE(HDRP(bp)) && !GET_ALLOC(HDRP(bp))){
+				return bp;
+			}
+		}
+		index ++;
+	}
+
+	return NULL;
 }
 
 static void place(void *bp, size_t asize){
     size_t csize = GET_SIZE(HDRP(bp));
+    deleteFromFreeList(bp);
 
     if((csize - asize) >= (2*DSIZE)) {
-        PUT(HDRP(bp), PACK(asize, 1));
-        PUT(FTRP(bp), PACK(asize, 1));
+        PUT(HDRP(bp), PACK(asize, GET_ALLOC_PREV_BLOCK(bp)|1));
+        PUT(FTRP(bp), PACK(asize, GET_ALLOC_PREV_BLOCK(bp)|1));
         bp = NEXT_BLKP(bp);
-        PUT(HDRP(bp), PACK(csize-asize,0));
-        PUT(FTRP(bp), PACK(csize-asize,0));
+        PUT(HDRP(bp), PACK(csize-asize,2));
+        PUT(FTRP(bp), PACK(csize-asize,2));
+        addToFreeList(bp);
     }
     else{
-
-        PUT(HDRP(bp), PACK(csize,1));
-        PUT(FTRP(bp), PACK(csize,1));
+        PUT(HDRP(bp), PACK(csize,GET_ALLOC_PREV_BLOCK(bp)|1));
+        if(NEXT_BLKP(bp))
+			PUT(HDRP(NEXT_BLKP(bp)),(GET(HDRP(NEXT_BLKP(bp)))|2));
     }
 }
 
@@ -176,7 +202,7 @@ void *mm_malloc(size_t size)
     if(size==0)
         return NULL;
     
-    if(size < DSIZE)
+    if(size <= DSIZE)
         asize = 2 * DSIZE;
     else
         asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
@@ -250,7 +276,82 @@ void *mm_realloc(void *ptr, size_t size)
 	return newptr;
 }
 
+static void deleteFromFreeList(void *bp)
+{
+	int index = indexOfFreeListArray(GET_SIZE(HDRP(bp)));
 
+	//CASE 1 : Delete from the beginning of free list
+	if(bp == freeListArray[index])
+	{
+		int offsetValue = GET(NEXT_PTR(bp));
+
+		if(0==offsetValue)
+		{
+			freeListArray[index] = NULL;
+			PUT(NEXT_PTR(bp),0);
+			PUT(PREV_PTR(bp),0);
+		}	
+
+		else if(offsetValue)
+		{
+			void *bp_nextBlock = actualAddressFromOffset(offsetValue);
+			freeListArray[index] = bp_nextBlock;
+
+			PUT(PREV_PTR(bp_nextBlock),0);
+			PUT(NEXT_PTR(bp),0);
+		}
+	}
+
+	// CASE 2 : Delete from the end of the free list
+	else if(0 == GET(NEXT_PTR(bp)))
+	{
+		int offsetValue = GET(PREV_PTR(bp));
+
+		if(offsetValue)
+		{
+			void *bp_prevBlock = actualAddressFromOffset(offsetValue);
+			PUT(NEXT_PTR(bp_prevBlock),0);
+		}
+
+		PUT(PREV_PTR(bp),0);
+	}
+
+	// CASE 3 : Delete from middle of the free list
+	else
+	{	
+		if((GET(PREV_PTR(bp))!=0) && (GET(NEXT_PTR(bp))!=0))
+		{
+			void *bp_prevBlock = actualAddressFromOffset(GET(PREV_PTR(bp)));
+			void *bp_nextBlock = actualAddressFromOffset(GET(NEXT_PTR(bp)));
+
+			PUT(NEXT_PTR(bp_prevBlock), GET(NEXT_PTR(bp)));
+			PUT(PREV_PTR(bp_nextBlock), GET(PREV_PTR(bp)));
+			PUT(NEXT_PTR(bp),0);
+			PUT(PREV_PTR(bp),0);
+		}
+
+	}
+	//mm_checkheap(579);
+}
+
+static void addToFreeList(void *bp)
+{
+	int index = indexOfFreeListArray(GET_SIZE(HDRP(bp)));
+
+	// If there is no block in free list
+	if(!freeListArray[index])
+		PUT(NEXT_PTR(bp),0);
+
+	// Put block in the beginning of the free list
+	else
+	{
+		PUT(NEXT_PTR(bp),offsetFromActualAddress(freeListArray[index]));
+		PUT(PREV_PTR(freeListArray[index]),offsetFromActualAddress(bp));
+	}
+
+	freeListArray[index] = bp;
+	PUT(PREV_PTR(bp),0);
+}
 
 
 
